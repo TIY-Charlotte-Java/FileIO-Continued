2566ff8c5a1a4bdb0ce8491bf07b2047175cc1a4
Michael Grill <Ultramar@michaels-mbp.apollosrc.root>
2016-04-08 14:13:23 -0400

    NotSoMuchProgress

--- src/com/theironyard/clt/Main.java
+++ src/com/theironyard/clt/Main.java
@@ -9,13 +9,14 @@ public class Main {
 
     public static void main(String[] args)throws FileNotFoundException {
 	// write your code here
-        HashMap<String, ArrayList<Country>> map = new HashMap<>();
+        HashMap<String, ArrayList<Country>> map;
         System.out.println("Please enter a letter for the search.");
         Scanner consoleScanner = new Scanner(System.in);
         Scanner fileScanner = new Scanner("countries.txt");
         while (fileScanner.hasNext()) {
             String line = fileScanner.nextLine();
             String[] columns = line.split("\\|");
+
         }
 
 
