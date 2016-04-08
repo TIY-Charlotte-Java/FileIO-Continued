2cbfcf0766243c10d3dc8c038ca8936c646c4df7
Michael Grill <Ultramar@michaels-mbp.apollosrc.root>
2016-04-08 11:53:48 -0400

    Friday

---
+++ src/com/theironyard/clt/Country.java
@@ -0,0 +1,12 @@
+package com.theironyard.clt;
+
+
+public class Country {
+    public String abbr;
+    public String name;
+
+    Country(String abbr, String name) {
+        this.abbr = abbr;
+        this.name = name;
+    }
+}
--- src/com/theironyard/clt/Main.java
+++ src/com/theironyard/clt/Main.java
@@ -1,8 +1,16 @@
 package com.theironyard.clt;
 
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)throws FileNotFoundException {
 	// write your code here
+        HashMap<String, ArrayList<Country>> states = new HashMap<>();
+
+
     }
 }
