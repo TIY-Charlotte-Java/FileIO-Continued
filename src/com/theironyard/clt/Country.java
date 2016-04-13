878fb4d55df4671d7ae36899bdb7fe685ead4d08
crenny288 <crenny288@gmail.com>
2016-04-13 09:23:56 -0400

    redo FileIO continuation -the second to the last_crappy one

--- src/com/theironyard/clt/Country.java
+++ src/com/theironyard/clt/Country.java
@@ -4,8 +4,8 @@ package com.theironyard.clt;
  * Created by mac on 4/12/16.
  */
 public class Country {
-    private String abbreviation;
-    private String name;
+    public String abbreviation;
+    public String name;
 
     public Country(String abbreviation, String name) {
         this.abbreviation = abbreviation;
--- src/com/theironyard/clt/Main.java
+++ src/com/theironyard/clt/Main.java
@@ -4,48 +4,50 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Main {
-    public static void main(String[] args) throws IOException
-{
-        // create ArrayList to store Countries
-        ArrayList<Country> countries = new ArrayList<>();
-
-        // create a Buffered Reader object instance with a FileReader
-        BufferedReader br = new BufferedReader(new FileReader("countries.txt"));
-
-        //read the first line from the text file
-        String fileRead = br.readLine();
-
-        // loop until all lines are read
-        while (fileRead != null)
-        {
-            //use string.split to load a string array with the values from each
-            //the file, using a pipe as he delimiter
-            String[] tokenize = fileRead.split("\\|");
-
-            String abbreviation = tokenize[0];
-            String name = tokenize[1];
-
-            Country tempObj = new Country(abbreviation, name);
-
-            //add to array list
-            countries.add(tempObj);
-
-            //read next line before looping
-            fileRead = br.readLine();
-
-        }
-
-        // close file stream
-        br.close();
-
+    public static void main(String[] args) throws IOException {
 
 
     }
 
+    // create ArrayList to store Countries
+    ArrayList<Country> countries = new ArrayList<>();
 
+    // create a Buffered Reader object instance with a FileReader
+    BufferedReader br = new BufferedReader(new FileReader("countries.txt"));
+
+    //read the first line from the text file
+    String fileRead = br.readLine();
+
+    // loop until all lines are read
+    while(fileRead != null);
+
+
+    {
+        //use string.split to load a string array with the values from each
+        //the file, using a pipe as he delimiter
+        String[] tokenize = fileRead.split("\\|");
+
+        String abbreviation = tokenize[0];
+        String name = tokenize[1];
+
+        Country tempObj = new Country(abbreviation, name);
+
+        //add to array list
+        countries.add(tempObj);
+
+        //read next line before looping
+        fileRead = br.readLine();
+
+    }
+
+    // close file stream
+    br.close();
 }
+
+
 
 //        //@Override
 //        private ArrayList<Countries> getCountries() {
@@ -57,7 +59,6 @@ public class Main {
 //
 //            countries = new ArrayList<>();
 //
-//            // if (File.exists(countriesPath))  // prevent the FileNotFoundException
 //            {
 //                try (BufferedReader in = new BufferedReader(new FileReader(countriesFile))) {
 //                    String line = in.readLine();
@@ -69,8 +70,8 @@ public class Main {
 //                        }
 //
 //                        String[] columns = FIELD_SEP.split(line);
-//                        String firstName = columns[0];
-//                        String lastName = columns[1];
+//                        String abbreviation = columns[0];
+//                        String name = columns[1];
 //
 //                        Countries c = new Countries(abbreviation, name);
 //                        countries.add(c);
@@ -82,13 +83,26 @@ public class Main {
 //                }
 //
 //            }
+
 //
 //
+//    HashMap<String, ArrayList<Country>> map = new HashMap<>();
+//    String line = "Abbreviation|Country";
+//    String[] data=line.split("\\|");
+//    Country countries = new Country(data[0], data[1]);
+//    Character key = countries.name.charAt(0);
+//map.put(key.toString().toUpperCase());
+//
+//        if (map.get(key) == null) {
+//        map.put(key, new ArrayList<country>());
 //
 //        }
-//
-//    }
-//
+
+
+        }
+
+}
+
 	// write your code here
 
 
